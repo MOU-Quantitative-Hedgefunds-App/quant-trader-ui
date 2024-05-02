1774fa8b8429c6d330ff46933e58618650485b2f
David Zamora <d.zamorac@uniandes.edu.co>
2024-05-01 22:37:04 -0500

    svelte fuera

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { goto } from '$app/navigation';
-    import '../app.css';
-  </script>
-  
-  <h1>Welcome to SvelteKit</h1>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-  <button on:click={() => goto('/cuentas')}>Ir a Cuentas</button>
-  
